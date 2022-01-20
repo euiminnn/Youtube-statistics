db2eeabb0e6a2fcb17c243036b1591516763ce12
Euimin Chung <jane3079@korea.ac.kr>
2022-01-21 04:48:47 +0900

    refactor: remove redundant modules

--- statistics_withoutAPI.py
+++ statistics_withoutAPI.py
@@ -1,13 +1,15 @@
 from googleapiclient.discovery import build
 
 DEVELOPER_KEY = "PUTAPIKEYHERE"
+channel_name = "PUTCHANNELNAMEHERE"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
 search_response = youtube.search().list(
-        q = "PUTCHANNELNAMEHERE",
+        q = channel_name,
+        type = "channel",
         order = "relevance",
         part = "snippet",
         maxResults = 50
@@ -18,76 +20,52 @@ channel_id = search_response['items'][0]['id']['channelId']
 playlists = youtube.playlists().list(
         channelId = channel_id,
         part = "snippet",
-        maxResults = 20
+        maxResults = 50
         ).execute()
 
-import pandas as pd
-
 ids = []
-titles = []
 for i in playlists['items'] :
     ids.append(i['id'])
-    titles.append(i['snippet']['title'])
-
-df=pd.DataFrame([ids, titles]).T
-df.columns=['PlayLists', 'Titles']
-
 
 num=int(input())
-playlist_selected=df['PlayLists'][num]
-playlist_videos=youtube.playlistItems().list(
+if num > (len(ids) - 1) or num < 0 :
+        print("Input a playlist index of %s." %channel_name)
+        print("The range of the input is from %d to %d." % (0, len(ids)-1))
+        exit()
+playlist_selected=ids[num]
+playlistitems_list_response = youtube.playlistItems().list(
         playlistId= playlist_selected,
         part = "snippet",
         maxResults = 50,
-        )
-playlistitems_list_response = playlist_videos.execute()
+        ).execute()
 
-video_names = []
 video_ids = []
-date = []
-
 for v in playlistitems_list_response['items'] :
-    video_names.append(v['snippet']['title'])
     video_ids.append(v['snippet']['resourceId']['videoId'])
-    date.append(v['snippet']['publishedAt'])
-vdf = pd.DataFrame([date, video_names, video_ids]).T
-vdf.columns = ['Date', 'Title', 'IDS']
 
-import re
-category_id = []
+title = []
 views = []
 likes = []
-dislikes = []
 comments = []
-mins = []
-seconds = []
-title = []
 
-for u in range(len(vdf)):
+for u in range(len(video_ids)):
     request = youtube.videos().list(
-            part = "snippet, contentDetails, statistics",
-            id = vdf['IDS'][u]
+            part = "snippet, statistics",
+            id = video_ids[u]
             )
     response = request.execute()
 
-    if response['items'] == [] :
-        ids.append("-")
-        category_id.append("-")
-        views.append("-")
-        likes.append("-")
-        #dislikes.append("-")
-        comments.append("-")
-    else :
+    if not response['items'] == [] :
         title.append(response['items'][0]['snippet']['title'])
-        category_id.append(response['items'][0]['snippet']['categoryId'])
         views.append(response['items'][0]['statistics']['viewCount'])
         likes.append(response['items'][0]['statistics']['likeCount'])
-        #dislikes.append(response['items'][0]['statistics']['dislikeCount'])
         comments.append(response['items'][0]['statistics']['commentCount'])
 
-sdf = pd.DataFrame([title, category_id, views, likes, comments]).T
-sdf.columns = ['Title', 'Category_id', 'Views', 'Likes', 'Comments']
+import pandas as pd
+
+df = pd.DataFrame([title, views, likes, comments]).T
+df.columns = ['Title', 'Views', 'Likes', 'Comments']
 
 from tabulate import tabulate
 
-print(tabulate(sdf, headers='keys', tablefmt='psql'))
+print(tabulate(df, headers='keys', tablefmt='psql'))
